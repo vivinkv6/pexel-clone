3d6628a906f8a7e9de76bef40447f0fddf3c0fae
vivinkv6 <vivinkv0@gmail.com>
2023-10-31 21:08:39 +0530

    update banner component

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Free Stock Photos Royalty free images and video shared by creators' });
+  res.render('index', { title: 'Free Stock Photos Royalty free images shared by creators',img:'https://images.pexels.com/photos/18760211/pexels-photo-18760211.jpeg' });
 });
+
+router.get('/videos',function(req,res) {
+  res.render('pages/videos',{title:'Free Stock Videos Royalty free videos shared by creators',video:'https://player.vimeo.com/progressive_redirect/playback/850743621/rendition/360p/file.mp4?loc=external&oauth2_token_id=57447761&signature=3bd92faae4d3e3d58d081c4dee9bb31cd0e64fc1d4c9fb5f1deebee26961762c'})
+})
 
 module.exports = router;
